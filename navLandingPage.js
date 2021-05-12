b20489cfc5f7e811fbd15aafca74ad6ae8ea560a
Giyu <80783085+c1am@users.noreply.github.com>
2021-05-11 20:18:31 -0400

    changes to API fetch

--- navLandingPage.js
+++ navLandingPage.js
@@ -1,1 +1,10 @@
+fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=true')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    })
+    .catch ((err) => {
 
+    })
