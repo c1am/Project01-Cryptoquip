b1fa72825c912ba0c2d6abcc42d0d4bef6c17396
Giyu <80783085+c1am@users.noreply.github.com>
2021-05-13 21:36:51 -0400

    add button

--- navLandingPage.js
+++ navLandingPage.js
@@ -1,7 +1,49 @@
+var addBtnEl = document.getElementById("addBtn")
+var nameInputEl = document.getElementById("coinInput")
+
+function handleAdd(event) {
+    var cryptoArray = [];
+    var cryptocurrency = nameInputEl.value.trim();
+    console.log(cryptocurrency);
+
+    event.preventDefault();
+
+    checkCoinName(cryptocurrency);
+
+    if (coinNameStatus = true) {
+        cryptoArray
+    }
+};
+
+function checkCoinName(coinName) {
+    var id = coinName
+    var coinNameStatus = false
+    var apiUrl = 'https://api.coingecko.com/api/v3/coins/' + coinName ;
+
+    fetch (apiUrl)
+        .then((response) => {
+            if (response.ok) {
+                console.log(response);
+                return response.json();
+            } else {
+                alert('Error: ' + response.statusText);
+            };
+
+            if (coinName = data.name) {
+                return coinNameStatus = true;
+            } else {
+                alert("Can't find this cryptocurrency!");
+            };
+        })
+
+        .catch(function (error) {
+            alert('Unable to connect to the API.');
+        });
+};
+  
 
 
-
-fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=true')
+/* fetch('https://api.coingecko.com/api/v3/coins/ethereum')
     .then((response) => {
         return response.json()
     })
@@ -22,4 +64,6 @@ fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=true')
             // if available, the platform
     })
     .catch ((err) => {
-    })
+    }) */
+
+addBtnEl.addEventListener('click', handleAdd);
